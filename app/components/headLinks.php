b69bd3be20d35dbc05b017b1cc1c81a5b5451997
ThePope05 <swobben05@gmail.com>
2023-10-26 12:47:09 +0200

    Added head links component

---
+++ app/components/headLinks.php
@@ -0,0 +1,19 @@
+<?php
+
+class Headlinks
+{
+
+    public function __construct()
+    {
+        $element = '
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+        ';
+
+        define('HEADLINKS', $element);
+    }
+}
+
+new Headlinks();
