b13094f4805682b3924a2fce1bb54f85077e2013
ThePope05 <swobben05@gmail.com>
2023-10-26 12:47:40 +0200

    Added auto loader for components

--- app/require.php
+++ app/require.php
@@ -4,6 +4,11 @@ require_once 'libraries/BaseController.php';
 require_once 'libraries/Database.php';
 require_once 'config/config.php';
 
-require_once 'components/navbar.php';
+//require all component files in the components folder
+$components = glob(__DIR__ . '/components/*');
+
+foreach ($components as $component) {
+    require_once($component);
+}
 
 $init = new Core();
