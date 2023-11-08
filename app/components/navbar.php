d22f537cd59c2a36c83ec8f494a0461896685ebf
ThePope05 <swobben05@gmail.com>
2023-11-08 13:23:52 +0100

    Added dashboard and logout to navbar
    Only visible if user is logged in

--- app/components/navbar.php
+++ app/components/navbar.php
@@ -8,9 +8,13 @@ class Navbar
         $element = "
         <nav>
             <img id='navLogo' src='/img/Rocambolesque-logo-DEF.jpg'>
-            <div id='right'>
-                <a class='navButton' href='/User/loginPage'>Login</a>
-                <a class='navButton' href='/User/signUpPage'>Register</a>
+            <div id='right'>" .
+            (isset($_SESSION['user_id']) ? "
+            <a class='navButton' href='/'>Dashboard</a>" : "
+            <a class='navButton' href='/User/loginPage'>Login</a>") .
+            (isset($_SESSION['user_id']) ? "
+            <a class='navButton' href='/User/logout'>Logout</a>" : "
+                <a class='navButton' href='/User/signUpPage'>Register</a>") . "
             </div>
         </nav>";
 
