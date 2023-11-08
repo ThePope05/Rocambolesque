d41be828ea69c77014d2426c3ef947e49145064b
ThePope05 <swobben05@gmail.com>
2023-11-08 12:45:12 +0100

    Added config-example file

---
+++ app/config/config-example.php
@@ -0,0 +1,10 @@
+<?php
+
+//Create local config.php
+//copy this code into that file
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Rocambolesque');
+define('URLROOT', 'www.Rocambolesque.com');
