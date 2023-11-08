309f29fbfbc2af9d738f0755f9cae37b5e239a37
ThePope05 <swobben05@gmail.com>
2023-11-08 14:03:32 +0100

    Updated navbar logo
    Img needs to be restyled, had to be wrapped in a tag for homepage link

--- app/components/navbar.php
+++ app/components/navbar.php
@@ -5,15 +5,17 @@ class Navbar
 
     public function __construct()
     {
+        //Problem refresh page 2 times before navbar is updated according to session id
+
         $element = "
         <nav>
-            <img id='navLogo' src='/img/Rocambolesque-logo-DEF.jpg'>
+            <a class='navButton' href='/'><img id='navLogo' src='/img/Rocambolesque-logo-DEF.jpg'></a>
             <div id='right'>" .
             (isset($_SESSION['user_id']) ? "
-            <a class='navButton' href='/'>Dashboard</a>" : "
-            <a class='navButton' href='/User/loginPage'>Login</a>") .
+                <a class='navButton' href='/'>Dashboard</a>" : "
+                <a class='navButton' href='/User/loginPage'>Login</a>") .
             (isset($_SESSION['user_id']) ? "
-            <a class='navButton' href='/User/logout'>Logout</a>" : "
+                <a class='navButton' href='/User/logout'>Logout</a>" : "
                 <a class='navButton' href='/User/signUpPage'>Register</a>") . "
             </div>
         </nav>";
