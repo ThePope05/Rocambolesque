08c8564b55909ada78e67e11416721472277b8e7
ThePope05 <swobben05@gmail.com>
2023-11-15 13:43:20 +0100

    Updated components loading system

---
+++ app/views/components/navbar.php
@@ -0,0 +1,12 @@
+<nav>
+    <a class='navButton' href='/'><img id='navLogo' src='/img/Rocambolesque-logo-DEF.jpg'></a>
+    <div id='right'>
+        <?php echo (isset($_SESSION['user_id']) ? "
+        <a class='navButton' href='/'>Dashboard</a>
+        <a class='navButton' href='/User/logout'>Logout</a>"
+            : "
+        <a class='navButton' href='/User/loginPage'>Login</a>
+        <a class='navButton' href='/User/signUpPage'>Register</a>")
+        ?>
+    </div>
+</nav>
