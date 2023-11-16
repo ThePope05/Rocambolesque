50401d37c0aa75e45254ea17efc08fe1508fb39e
wybren33 <335480@student.mboutrecht.nl>
2023-11-16 11:08:09 +0100

    homepage and styling restored

--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -7,7 +7,18 @@
 </head>
 
 <body>
+<<<<<<< Updated upstream
     <?php $this->components('navbar'); ?>
+=======
+    <?= NAVBAR; ?>
+    <img id="background-img" src="img/Restaurant1.jpg">
+    <div class="Boxcontainer">
+        <h3 class="Boxtitle">Menu</h3>
+    </div>
+    <div class="Boxcontainer">
+        <h3 class="Boxtitle">Reservering</h3>
+    </div>
+>>>>>>> Stashed changes
 </body>
 
 </html>
