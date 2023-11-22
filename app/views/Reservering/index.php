67ee6474c9e7a40ed665a59d386b29ff5770a369
ThePope05 <swobben05@gmail.com>
2023-11-22 12:44:59 +0100

    Added basics for reservering page

--- app/views/Reservering/index.php
+++ app/views/Reservering/index.php
@@ -9,6 +9,13 @@
 <body>
     <?php $this->components('navbar'); ?>
 
+    <?php $action = (isset($_SESSION['user_id']) ? "/reservering" : "/user"); ?>
+    <!-- If user logged in, add reservering, else ask user info first -->
+    <h1>Reservering</h1>
+    <form action="<?= $action ?>" method="post">
+
+    </form>
+
 </body>
 
 </html>
