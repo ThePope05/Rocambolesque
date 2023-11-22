05b106167dd393a184862ae98d29b9bbe17f66b3
wybren33 <335480@student.mboutrecht.nl>
2023-11-22 13:15:09 +0100

    final commit homepage

--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -13,10 +13,17 @@
     </div>
     <div class="Boxcontainer">
         <h3 class="Boxtitle">Menu</h3>
-        <a href="/menu">Menu</a>
+        <p>bekijk ons menu <br> hier 
+        <br> <span id="arrow-icon" class="material-symbols-rounded">arrow_downward</span>
+        </p>
+        <a id="homeButtons" href="/menu">Menu</a>
     </div>
     <div class="Boxcontainer">
         <h3 class="Boxtitle">Reservering</h3>
+        <p>U kunt hier <br> Reserveren 
+        <br> <span id="arrow-icon" class="material-symbols-rounded">arrow_downward</span>
+        </p>
+        <a id="homeButtons" href="/reservering">reservering</a>
     </div>
 </body>
 
--- app/views/components/headLinks.php
+++ app/views/components/headLinks.php
@@ -4,4 +4,5 @@
 <link rel='preconnect' href='https://fonts.googleapis.com'>
 <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
 <link href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap' rel='stylesheet'>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
 <link rel='stylesheet' href='/css/style.css'>
