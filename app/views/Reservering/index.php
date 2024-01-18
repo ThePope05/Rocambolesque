05e779bf34cb1c8ab068edd9f087583fe58cee49
ThePope05 <swobben05@gmail.com>
2024-01-18 11:06:01 +0100

    Fixed Tim's stupid code
    Sort of

--- app/views/Reservering/index.php
+++ app/views/Reservering/index.php
@@ -17,72 +17,59 @@
                     <?php endif; ?>
 
                     <!-- show latest reservation -->
-                    <?php
-                    // if user is logged in
-                    if (isset($_SESSION['user_id'])) {
-                        if (count($data['reservation']) > 0) {
-                            echo "<h1>Latest Reservation</h1>";
-                            echo "<table class='table'>";
-                            echo "<thead>";
-                            echo "<tr>";
-                            echo "<th scope='col'>#</th>";
-                            echo "<th scope='col'>Amount of People</th>";
-                            echo "<th scope='col'>Amount of Children</th>";
-                            echo "<th scope='col'>Reservation Time</th>";
-                            echo "<th scope='col'>Comment</th>";
-                            echo "<th scope='col'>User ID</th>";
-                            echo "<th scope='col'>Delete</th>";
-                            echo "</tr>";
-                            echo "</thead>";
-                            echo "<tbody>";
-                            foreach ($data['reservation'] as $reservation) {
-                                echo "<tr>";
-                                echo "<th scope='row'>" . $reservation->Id . "</th>";
-                                echo "<td>" . $reservation->AmountOfPeople . "</td>";
-                                echo "<td>" . $reservation->AmountOfChildren . "</td>";
-                                echo "<td>" . $reservation->ReservationTime . "</td>";
-                                echo "<td>" . $reservation->Comment . "</td>";
-                                echo "<td>" . $reservation->user_id . "</td>";
-                                echo "<td><a href='/reservering/delete/" . $reservation->Id . "' class='btn btn-danger'>Delete</a></td>";
-                                echo "</tr>";
+                    <h1>Latest Reservation</h1>
+                    <table class='table'>
+                        <thead>
+                            <tr>
+                                <th scope='col'>#</th>
+                                <th scope='col'>Amount of People</th>
+                                <th scope='col'>Amount of Children</th>
+                                <th scope='col'>Reservation Time</th>
+                                <th scope='col'>Comment</th>
+                                <th scope='col'>User ID</th>
+                            </tr>
+                        </thead>
+                        <?php
+                        // if user is logged in
+                        if (isset($_SESSION['user_id'])) {
+                            if (count($data['reservation']) > 0) {
+                                echo "<tbody>";
+                                foreach ($data['reservation'] as $reservation) {
+                                    echo "<tr>";
+                                    echo "<th scope='row'>" . $reservation->Id . "</th>";
+                                    echo "<td>" . $reservation->AmountOfPeople . "</td>";
+                                    echo "<td>" . $reservation->AmountOfChildren . "</td>";
+                                    echo "<td>" . $reservation->ReservationTime . "</td>";
+                                    echo "<td>" . $reservation->Comment . "</td>";
+                                    echo "<td>" . $reservation->user_id . "</td>";
+                                    echo "<td><a href='/reservering/delete/" . $reservation->Id . "' class='btn btn-danger'>Delete</a></td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";
+                                echo "</table>";
+                            } else {
+                                echo "<h1>No Reservations</h1>";
                             }
-                            echo "</tbody>";
-                            echo "</table>";
                         } else {
-                            echo "<h1>No Reservations</h1>";
-                        }
-                    } else {
-                        // if user is not logged in
-                        if (count($data['reservation']) > 0) {
-                            echo "<h1>Latest Reservation</h1>";
-                            echo "<table class='table'>";
-                            echo "<thead>";
-                            echo "<tr>";
-                            echo "<th scope='col'>#</th>";
-                            echo "<th scope='col'>Amount of People</th>";
-                            echo "<th scope='col'>Amount of Children</th>";
-                            echo "<th scope='col'>Reservation Time</th>";
-                            echo "<th scope='col'>Comment</th>";
-                            echo "<th scope='col'>User ID</th>";
-                            echo "</tr>";
-                            echo "</thead>";
-                            echo "<tbody>";
-                            foreach ($data['reservation'] as $reservation) {
-                                echo "<tr>";
-                                echo "<th scope='row'>" . $reservation->user_id . "</th>";
-                                echo "<td>" . $reservation->AmountOfPeople . "</td>";
-                                echo "<td>" . $reservation->AmountOfChildren . "</td>";
-                                echo "<td>" . $reservation->ReservationTime . "</td>";
-                                echo "<td>" . $reservation->Comment . "</td>";
-                                echo "</tr>";
+                            // if user is not logged in
+                            if (count($data['reservation']) > 0) {
+                                echo "<tbody>";
+                                foreach ($data['reservation'] as $reservation) {
+                                    echo "<tr>";
+                                    echo "<th scope='row'>" . $reservation->user_id . "</th>";
+                                    echo "<td>" . $reservation->AmountOfPeople . "</td>";
+                                    echo "<td>" . $reservation->AmountOfChildren . "</td>";
+                                    echo "<td>" . $reservation->ReservationTime . "</td>";
+                                    echo "<td>" . $reservation->Comment . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";
+                                echo "</table>";
+                            } else {
+                                echo "<h1>No Reservations</h1>";
                             }
-                            echo "</tbody>";
-                            echo "</table>";
-                        } else {
-                            echo "<h1>No Reservations</h1>";
                         }
-                    }
-                    ?>
+                        ?>
                 </div>
             </div>
         </div>
