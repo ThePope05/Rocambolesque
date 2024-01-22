1a3c15bee4b7b35a089425775dbae7e90ad30e51
ThePope05 <swobben05@gmail.com>
2024-01-22 18:23:21 +0100

    Updated general styling
    SHIT DOES NOT DISAPEAR INTO THE VOID CALLED NAVBAR ANYMORE (Don't ever do this again @wybren33)

--- app/views/Reservering/index.php
+++ app/views/Reservering/index.php
@@ -8,65 +8,58 @@
 
 <body>
     <?php $this->components('navbar'); ?>
-    <div class="content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <!-- show latest reservation -->
-                    <h1>Latest Reservation</h1>
-                    <table class='table'>
-                        <thead>
-                            <tr>
-                                <th scope='col'>#</th>
-                                <th scope='col'>Amount of People</th>
-                                <th scope='col'>Amount of Children</th>
-                                <th scope='col'>Reservation Time</th>
-                                <th scope='col'>Comment</th>
-                                <th scope='col'>User ID</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (is_array($data['reservation']) && count($data['reservation']) > 0) : ?>
-                                <?php foreach ($data['reservation'] as $reservation) : ?>
-                                    <tr>
-                                        <th scope='row'><?= $reservation->Id; ?></th>
-                                        <td><?= $reservation->AmountOfPeople; ?></td>
-                                        <td><?= $reservation->AmountOfChildren; ?></td>
-                                        <td><?= $reservation->ReservationTime; ?></td>
-                                        <td><?= $reservation->Comment; ?></td>
-                                        <td><?= $reservation->user_id; ?></td>
-                                        <td><a href='/reservering/updatePage/<?= $reservation->Id ?>' class='btn btn-primary'>
-                                                <span class="material-symbols-outlined">
-                                                    edit
-                                                </span>
-                                            </a></td>
-                                        <td><a href='/reservering/delete/<?= $reservation->Id ?>' class='btn btn-danger'>
-                                                <span class="material-symbols-outlined">
-                                                    delete
-                                                </span></a></td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            <?php else : ?>
-                                <tr>
-                                    <th scope='row'></th>
-                                    <td>No</td>
-                                    <td>reservations</td>
-                                    <td>were found</td>
-                                </tr>
-                            <?php endif; ?>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <th><a href="/reservering/createPage"><span class="material-symbols-outlined">
-                                            add
-                                        </span></a></th>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+    <div class="container">
+        <h1>Latest Reservation</h1>
+        <table class='table'>
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Amount of People</th>
+                    <th scope='col'>Amount of Children</th>
+                    <th scope='col'>Reservation Time</th>
+                    <th scope='col'>Comment</th>
+                    <th scope='col'>User ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (is_array($data['reservation']) && count($data['reservation']) > 0) : ?>
+                    <?php foreach ($data['reservation'] as $reservation) : ?>
+                        <tr>
+                            <th scope='row'><?= $reservation->Id; ?></th>
+                            <td><?= $reservation->AmountOfPeople; ?></td>
+                            <td><?= $reservation->AmountOfChildren; ?></td>
+                            <td><?= $reservation->ReservationTime; ?></td>
+                            <td><?= $reservation->Comment; ?></td>
+                            <td><?= $reservation->user_id; ?></td>
+                            <td><a href='/reservering/updatePage/<?= $reservation->Id ?>' class='btn btn-primary'>
+                                    <span class="material-symbols-outlined">
+                                        edit
+                                    </span>
+                                </a></td>
+                            <td><a href='/reservering/delete/<?= $reservation->Id ?>' class='btn btn-danger'>
+                                    <span class="material-symbols-outlined">
+                                        delete
+                                    </span></a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <tr>
+                        <th scope='row'></th>
+                        <td>No</td>
+                        <td>reservations</td>
+                        <td>were found</td>
+                    </tr>
+                <?php endif; ?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th><a href="/reservering/createPage"><span class="material-symbols-outlined">
+                                add
+                            </span></a></th>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 
