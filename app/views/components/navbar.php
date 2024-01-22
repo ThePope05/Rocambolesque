e83e32f88eb9c8a7a5c9f5232ffbc2a9366c537f
wybren33 <335480@student.mboutrecht.nl>
2024-01-22 20:12:30 +0100

    made button for contact page

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -2,9 +2,11 @@
     <a class='navButton' href='/'><img id='navLogo' src='/img/Rocambolesque-logo-DEF.jpg'></a>
     <div id='right'>
         <?php echo (isset($_SESSION['user_id']) ? "
+        <a class='navButton' href='app\views\Homepage\contact.php'>Contact</a>
         <a class='navButton' href='/Reservering/'>Dashboard</a>
         <a class='navButton' href='/User/logout'>Logout</a>"
             : "
+        <a class='navButton' href='app\views\Homepage\contact.php'>Contact</a>
         <a class='navButton' href='/User/loginPage'>Login</a>
         <a class='navButton' href='/User/signUpPage'>Register</a>")
         ?>
