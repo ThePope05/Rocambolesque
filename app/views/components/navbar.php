72bea3b170f9ca380038410618fc3a933384e9bb
ThePope05 <swobben05@gmail.com>
2024-01-22 21:20:06 +0100

    Updated navbar
    Admin will now be able to see edit menu button

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,14 +1,16 @@
 <nav>
     <a class='navButton' href='/'><img id='navLogo' src='/img/Rocambolesque-logo-DEF.jpg'></a>
     <div id='right'>
-        <?php echo (isset($_SESSION['user_id']) ? "
         <a class='navButton' href='/Homepage/contact'>Contact</a>
-        <a class='navButton' href='/Reservering/'>Dashboard</a>
-        <a class='navButton' href='/User/logout'>Logout</a>"
-            : "
-        <a class='navButton' href='/Homepage/contact'>Contact</a>
-        <a class='navButton' href='/User/loginPage'>Login</a>
-        <a class='navButton' href='/User/signUpPage'>Register</a>")
-        ?>
+        <?php if (isset($_SESSION['user_id'])) : ?>
+            <?php if ($_SESSION['user_id'] == 1) : ?>
+                <a class='navButton' href='/Menu/editPage'>Edit Menu</a>
+            <?php endif; ?>
+            <a class='navButton' href='/Reservering/'>Reservations</a>
+            <a class='navButton' href='/User/logout'>Logout</a>
+        <?php else : ?>
+            <a class='navButton' href='/User/loginPage'>Login</a>
+            <a class='navButton' href='/User/signUpPage'>Register</a>
+        <?php endif; ?>
     </div>
 </nav>
